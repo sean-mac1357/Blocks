94fa95c55b9eb7886d03a29f6c8c429a7995d1e0
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-10-10 03:00:04 -0400

    Added navigation views and titles

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("News")/*@END_MENU_TOKEN@*/ }.tag(1)
-            Text("Tab Content 2").tabItem { Text("Products") }.tag(2)
-            Text("Tab Content 3").tabItem { Text("Chat") }.tag(3)
+            NavigationView {
+                Text("Tab Content 1")
+                    .navigationTitle("News")
+            }
+            .tabItem { /*@START_MENU_TOKEN@*/Text("News")/*@END_MENU_TOKEN@*/ }.tag(1)
+            NavigationView {
+                Text("Tab Content 2")
+                    .navigationTitle("Products")
+            }
+            .tabItem { Text("Products") }.tag(2)
+            NavigationView {
+                Text("Tab Content 3")
+                    .navigationTitle("Chat")
+            }
+            .tabItem { Text("Chat") }.tag(3)
         }
     }
 }
