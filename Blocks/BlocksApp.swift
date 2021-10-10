9e262946592e917e9ac142f7a83f54b107fac1f3
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-10-10 01:27:22 -0400

    Initial Commit

---
+++ Blocks/BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksApp.swift
+//  Blocks
+//
+//  Created by Sean Maceachern on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
