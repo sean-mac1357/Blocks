1f2d1660d6f79a914d8bb0cfaad61d21582f67b0
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-10-11 01:26:43 -0400

    Created Article model and mock instances for News

---
+++ Blocks/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  Blocks
+//
+//  Created by Sean Maceachern on 10/11/21.
+//
+
+import Foundation
+
+struct Article {
+    let title: String
+    let date: Date
+    let detail: String
+    let smallImageName: String
+    let largeImageName: String
+}
