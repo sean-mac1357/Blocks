da716430b622c4d349aa1137a454213a1efeb0ec
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-10-10 23:48:51 -0400

    Created NewsCell with custom layout

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -13,9 +13,7 @@ struct ContentView: View {
             NavigationView {
                 List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
                     NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                        Text("Text")
-                            .font(.title2)
-                            .fontWeight(.bold)
+                        NewsCell()
                     }
                 }
                 .navigationTitle("News")
@@ -39,6 +37,20 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        NewsCell()
+            .previewLayout(.sizeThatFits)
         ContentView()
+    }
+}
+
+struct NewsCell: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Text")
+                .font(.title2)
+                .fontWeight(.bold)
+            Text(/*@START_MENU_TOKEN@*/"Tertiary Text"/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.gray)
+        }
     }
 }
